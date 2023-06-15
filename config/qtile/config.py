57e4bffb15f3efbe76935c7c100a64e000d854f9
RilCritch <rilcritch@gmail.com>
2023-06-14 21:25:35 -0500

    moved config options to modules.settings

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -55,30 +55,7 @@ def start_once():
     home = os.path.expanduser('~')
     subprocess.call([home + '/.config/qtile/scripts/autostart.sh'])
 
-# configuration options
-# dgroups_key_binder = None
-# dgroups_app_rules = []  # type: list
-# follow_mouse_focus = True -- figure out a way to toggle between settings
-# follow_mouse_focus = False
-# bring_front_click = False
-# cursor_warp = False
-# auto_fullscreen = True
-# focus_on_window_activation = "smart"
-# reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
-# auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-# wl_input_rules = None
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-# wmname = "LG3D"
+# @hook.subscribe.startup
+# def start_every():
+#     home = os.path.expanduser('~')
+#     subprocess.call([home + '/.config/qtile/scripts/start.sh'])
