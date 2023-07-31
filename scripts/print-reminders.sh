34cb976638cf3bffcd05354346ebbc0d7ac2ae9b
RilCritch <rilcritch@gmail.com>
2023-07-31 18:53:27 -0500

    tweaked with output to fit better with catppuccin

--- scripts/print-reminders.sh
+++ scripts/print-reminders.sh
@@ -5,15 +5,15 @@
 userfile="${HOME}/.local/share/reminders/${USER}-reminders.txt"
 
 if [ -f $userfile ]; then
-  # title
-  # lineacross | clr blue
-  figlet -k -f shadow "Reminders" | clr blue
-  # lineacross | clr blue
-  
-  # reminders
-  \cat -n $userfile | clr green
-  echo
+	# title
+	# lineacross | clr blue
+	figlet -k -f shadow "Reminders" | clr blue
+	# lineacross | clr blue
+
+	# reminders
+	\cat -n $userfile | clr magenta
+	echo
 else
-  echo "Error... user '${USER}' does not have a reminders file." | clr red
-  echo "Please run 'reminders.sh' to create reminders file"
+	echo "Error... user '${USER}' does not have a reminders file." | clr red
+	echo "Please run 'reminders.sh' to create reminders file"
 fi
