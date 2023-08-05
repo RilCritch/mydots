c4bfb4832fff1cb4c6300fc7c8e5e527fd0e5ae0
RilCritch <rilcritch@gmail.com>
2023-08-05 10:39:44 -0500

    added and setup modicator with catppuccin colors

---
+++ config/nvim/lazy-nvim/lua/plugins/modicator.lua
@@ -0,0 +1,25 @@
+return {
+  { -- changes color of current line number to match mode colors
+    "mawkler/modicator.nvim",
+    init = function()
+      -- catpuccin integration
+      local mocha = require("catppuccin.palettes").get_palette("mocha") -- retrieving catppuccin colors
+
+      -- setting highlights that modicator uses
+      vim.api.nvim_set_hl(0, "NormalMode", { fg = mocha.blue })
+      vim.api.nvim_set_hl(0, "InsertMode", { fg = mocha.green })
+      vim.api.nvim_set_hl(0, "VisualMode", { fg = mocha.mauve })
+      vim.api.nvim_set_hl(0, "CommandMode", { fg = mocha.peach })
+      vim.api.nvim_set_hl(0, "ReplaceMode", { fg = mocha.red })
+      vim.api.nvim_set_hl(0, "SelectMode", { fg = mocha.blue })
+      vim.api.nvim_set_hl(0, "TerminalMode", { fg = mocha.blue })
+      vim.api.nvim_set_hl(0, "TerminalNormalMode", { fg = mocha.blue })
+
+      -- and defualt number for current line
+      vim.api.nvim_set_hl(0, "CursorLineNr", { fg = mocha.blue })
+    end,
+    config = function()
+      require("modicator").setup()
+    end,
+  },
+}
