ac772e17240b1f77bb0e25cf86df6f7c2323b4d1
RilCritch <rilcritch@gmail.com>
2023-08-05 11:27:17 -0500

    consolodating similar plugins to one file

--- config/nvim/lazy-nvim/lua/plugins/color-utilities.lua
+++ config/nvim/lazy-nvim/lua/plugins/color-utilities.lua
@@ -2,21 +2,27 @@ return {
   { -- changes color of current line number to match mode colors
     "mawkler/modicator.nvim",
     init = function()
-      -- catpuccin integration
-      local mocha = require("catppuccin.palettes").get_palette("mocha") -- retrieving catppuccin colors
+      -- vim options required by modicator
+      local opt = vim.opt
+      opt.cursorline = true
+      opt.number = true
+      opt.termguicolors = true
 
-      -- setting highlights that modicator uses
-      vim.api.nvim_set_hl(0, "NormalMode", { fg = mocha.blue })
-      vim.api.nvim_set_hl(0, "InsertMode", { fg = mocha.green })
-      vim.api.nvim_set_hl(0, "VisualMode", { fg = mocha.mauve })
-      vim.api.nvim_set_hl(0, "CommandMode", { fg = mocha.peach })
-      vim.api.nvim_set_hl(0, "ReplaceMode", { fg = mocha.red })
-      vim.api.nvim_set_hl(0, "SelectMode", { fg = mocha.blue })
-      vim.api.nvim_set_hl(0, "TerminalMode", { fg = mocha.blue })
-      vim.api.nvim_set_hl(0, "TerminalNormalMode", { fg = mocha.blue })
+      -- catppuccin integration
+      local mocha = require("catppuccin.palettes").get_palette("mocha") -- retrieving catppuccin colors
+      local hl = vim.api.nvim_set_hl -- set modicator hls with catppuccin colors
+
+      hl(0, "NormalMode", { fg = mocha.blue })
+      hl(0, "InsertMode", { fg = mocha.green })
+      hl(0, "VisualMode", { fg = mocha.mauve })
+      hl(0, "CommandMode", { fg = mocha.peach })
+      hl(0, "ReplaceMode", { fg = mocha.red })
+      hl(0, "SelectMode", { fg = mocha.blue })
+      hl(0, "TerminalMode", { fg = mocha.blue })
+      hl(0, "TerminalNormalMode", { fg = mocha.blue })
 
       -- and defualt number for current line
-      vim.api.nvim_set_hl(0, "CursorLineNr", { fg = mocha.blue })
+      hl(0, "CursorLineNr", { fg = mocha.blue })
     end,
     config = function()
       require("modicator").setup()
@@ -26,5 +32,15 @@ return {
   { -- making delimiters easier to distinguish
     "HiPhish/rainbow-delimiters.nvim",
     -- extra configuration here if needed
+  },
+
+  { -- hightlight color values
+    "NvChad/nvim-colorizer.lua",
+    -- opts = {
+    --
+    -- },
+    config = function(_, opts)
+      require("colorizer").setup(opts)
+    end,
   },
 }
